ff4cb581cdee356e6f213c5d78df04edefd4b565
shimeng28 <shimeng.eric@gmail.com>
2020-06-06 17:00:21 +0800

    feat: add port bind listen

--- socket1/main.cpp
+++ socket1/main.cpp
@@ -8,12 +8,16 @@
 #ifdef WIN32
 #include <windows.h>
 #else
-#include <unistd.h>
+#include <unistd.h> // close
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <iostream>
+#include <arpa/inet.h>
+
+#define closesocket close
 #endif
 
+#include <iostream>
+#include <cstdlib>
 
 
 int main(int argc, const char * argv[]) {
@@ -22,16 +26,28 @@ int main(int argc, const char * argv[]) {
   WSAStartup(MAKEWORD(2, 2), &ws);
   #endif
 
-  for (int i = 0; i < 2000; i++) {
-    int sock = socket(AF_INET, SOCK_STREAM, 0);
-    if (sock == -1) {
-      std::cout << "Socket create failed" << std::endl;
-      return -1;
-    }
-    std::cout << "[" << sock << "]";
-//    closesocket(sock);
+  int sock = socket(AF_INET, SOCK_STREAM, 0);
+  if (sock == -1) {
+    std::cout << "Socket create failed" << std::endl;
+    return -1;
   }
-  std::cout << std::endl;
+  unsigned short port = 8080;
+  if (argc > 1) {
+    port = atoi(argv[1]);
+  }
+  sockaddr_in saddr;
+  saddr.sin_family = AF_INET;
+  saddr.sin_port = htons(port);
+  saddr.sin_addr.s_addr = htonl(0);
+  
+  if (bind(sock, (sockaddr*)(&saddr), sizeof(saddr)) != 0) {
+    std::cout << "bind port fail " << port << std::endl;
+    return -2;
+  } else {
+    std::cout << "bind success " << port << std::endl;
+  }
+  
+  listen(sock, 10);
   
   return 0;
 }
